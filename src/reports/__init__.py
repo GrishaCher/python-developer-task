9a200307a60f64ef7335ce77d0af779c1fc95a10
GrishaCher <grish.chernikow2016@yandex.ru>
2025-10-18 16:19:10 +0300

    last changes

--- src/reports/__init__.py
+++ src/reports/__init__.py
@@ -10,5 +10,5 @@ REPORTS = {
 def get_report(report_name: str) -> Union[AverageRatingReport]:
     if report_name not in REPORTS:
         raise ValueError(f'''Отчет '{report_name}' не найден.
-                         Доступные: {list(REPORTS.keys())}''')
+Доступные: {list(REPORTS.keys())}''')
     return REPORTS[report_name]()
